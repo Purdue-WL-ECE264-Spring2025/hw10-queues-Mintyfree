c668eaa9df069e5f6846c35bbe1c330205e71af0
Arunav Lamba <axl110027@gmail.com>
2025-04-02 08:52:38 -0400

    Added some changes to dequeue and enqueue

--- queue.c
+++ queue.c
@@ -3,13 +3,15 @@
 
 void enqueue(struct queue *q, struct game_state state) 
 {
-
+    uint64_t newData = serialize(state);
+    insert_at_tail(q, newData);
 }
 
 struct game_state dequeue(struct queue *q) 
 { 
-    
-    return (struct game_state){0}; 
+    struct game_state curr_state = deserialize(q);
+
+    return (struct game_state);
 }
 
 int number_of_moves(struct game_state start) 
@@ -19,19 +21,22 @@ int number_of_moves(struct game_state start)
     q->data.head->next = new_node(newData);
     enqueue(q, start);
     struct game_state state = start;
+    int rowCounter = 0;
+    int colCounter = 0;
     int num = 1;
 
     while (q->data.head->next != NULL)
     {
         state = dequeue(q);
-        if ((state.tiles[num]) == num)
+        if ((state.tiles[rowCounter][colCounter]) == num)
         {
             num++;
+            rowCounter++;
+            colCounter++;
             continue;
         }
         else
         {
-            for ()
         }
     }
     
